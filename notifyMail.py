69fc3d38ca9eb4f5afdda0c001ad1a81360c1b69
davinellulinvega <davinellulinvega@nyaldee.com>
2016-06-22 14:28:06 +0100

    Add the watch and notification script

---
+++ notifyMail.py
@@ -0,0 +1,66 @@
+#!/usr/bin/python2.7
+# -*- coding: utf-8 -*-
+__author__ = 'davinellulinvega'
+
+import pynotify
+import pyinotify
+from os import environ
+
+
+class MailEventHandler(pyinotify.ProcessEvent):
+    """
+    A simple handler that will send a notification upon reception of a new mail
+    """
+
+    def process_IN_CREATE(self, event):
+        """
+        React to the creation of a new file in the watched directory.
+        :param event: The event and its properties
+        :return:
+        """
+
+        # Get the file linked to the event
+        file_path = event.pathname
+
+        # Read the interesting information from the file
+        subject = "Subject: None"
+        fro = "From: Unknown"
+        with open(file_path, "r") as mail_file:
+            line = mail_file.readline()
+            while line != "":
+                # Get the subject and sender
+                if line.startswith("Subject"):
+                    subject = line
+                if line.startswith("From"):
+                    fro = line
+                # Read the next line
+                line = mail_file.readline()
+
+        # Initialize the notify module
+        if pynotify.init("Notify Mail"):
+
+            # Declare a new notification
+            n = pynotify.Notification("New message", "{}\n{}".format(fro, subject), "/usr/share/icons/Faenza/apps/48/mail-notification.png")
+            n.set_urgency(1)
+
+            # Show the notification
+            n.show()
+
+
+if __name__ == "__main__":
+
+    # Instantiate a watch manager
+    wm = pyinotify.WatchManager()
+
+    # Instantiate a notifier object
+    notifier = pyinotify.ThreadedNotifier(wm, MailEventHandler())
+
+    # Add the folder to watch
+    wdd = wm.add_watch("{}/.claws-mail/imapcache/".format(environ["HOME"]), pyinotify.IN_CREATE,rec=True)
+
+    try:
+        # Start the thread
+        notifier.start()
+
+    except:
+        notifier.stop()
